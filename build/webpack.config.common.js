3c95ad0def9b4547db9546d71adb001b4374e42a
pikachu <hooraypikachu@gmail.com>
2019-10-22 14:55:39 +0800

    hello world

---
+++ build/webpack.config.common.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, '../src/ts/index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.json', '.scss', '.glsl', '.js'],
+    plugins: [
+      new TsConfigPathsPlugin({
+        configFileName: path.resolve(__dirname, '../tsconfig.json')
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['awesome-typescript-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'raw-loader'
+      }
+    ]
+  }
+}
---
+++ build/webpack.config.dev.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const common = require('./webpack.config.common.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    port: 9999,
+    inline: true,
+    clientLogLevel: 'error'
+  }
+})
